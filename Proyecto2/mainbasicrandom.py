3726282f2075e28d95f001a9734f6ebaaee27c43
cralf <36905229+cralf@users.noreply.github.com>
2018-06-08 22:39:15 -0400

    Listo el randoomizado revisado para ver si dan los numeros
     revisado para ver si dan los numeros Carmichael

--- Proyecto2/mainbasicrandom.py
+++ Proyecto2/mainbasicrandom.py
@@ -1,1 +1,15 @@
 #mainbasicrandom
+import random
+
+def Aleatorio_Basico(n=int,m=int):
+	for i in range(1,m+1):
+		a=random.randint(1,n-1)
+		if a**(n-1)%n!=1:
+			return "El  numero es compuesto"
+		elif a**(n-1)%n==1:		
+			return "el numero es primo"
+    
+#Esto me imagino que va en el main
+N=int(input("Coloque el valor de n:"))
+M=int(input("Coloque el valor de m para indicar cuantas veces se repetira el experimento:"))
+print Aleatorio_Basico(N,M)
