00d2da02f70d2c6b2ef791b056125f47e1522b6d
cralf <36905229+cralf@users.noreply.github.com>
2018-06-08 22:37:03 -0400

    Completado y revisado el de tentativa

--- Proyecto2/maintentativedivision.py
+++ Proyecto2/maintentativedivision.py
@@ -1,1 +1,16 @@
 #maintentativedivision
+def Division_por_Tentativa(n=int):
+	primo=True
+	i=2
+	while i<=n**(0.5) and primo:
+		if n % i==0:
+			primo=False
+			return ("el numero",n, "es compuesto")
+		i=i+1
+
+	if primo==True:
+		return ("el numero",n, "es primo") 	
+
+#Esto va en el main
+n=int(input("Coloque el valor de n:"))
+print Division_por_Tentativa(n)		
